125dbd3c70c019b240bf22f82a06c8788d731c7b
fraangelo <angelo.francesco.mobile@gmail.com>
2014-12-09 03:10:36 +0100

    new Event almost done

--- meteoCalProj/src/main/java/bakingBeans/newEvent.java
+++ meteoCalProj/src/main/java/bakingBeans/newEvent.java
@@ -7,15 +7,17 @@ package bakingBeans;
 
 import EJB.interfaces.CalendarManager;
 import EJB.interfaces.EventManager;
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.List;
 import javax.inject.Named;
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import model.CalendarModel;
 import model.Event;
 import model.PrivateEvent;
+import model.PublicEvent;
 
 /**
  *
@@ -23,21 +25,34 @@ import model.PrivateEvent;
  */
 @Named(value = "newEvent")
 @ViewScoped
-public class newEvent {
+public class newEvent implements Serializable {
 
-    Event newEvent;
-
+    private static final long serialVersionUID = 1L;
+    
+    Event eventToCreate;
+    
+    String description;
+    String location;
+    boolean outdoor;
+    boolean publicAccess;
+    String title;
+    String startDate;
+    String endDate;
+    String startTime;
+    String endTime;
+    
     CalendarModel calendar;
 
     LoginBacking login;
-    
+
     java.util.Calendar startDateTime;
-    
     java.util.Calendar endDateTime;
-    
+
+
+
     @Inject
     CalendarManager calendarManager;
-    
+
     @Inject
     EventManager eventManager;
 
@@ -47,54 +62,129 @@ public class newEvent {
     public newEvent() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         login = (LoginBacking) facesContext.getApplication().evaluateExpressionGet(facesContext, "#{login}", LoginBacking.class);
-
-        newEvent = new PrivateEvent();
-        newEvent.setOwner(login.getCurrentUser());
     }
 
     public void setTitle(String title) {
-        newEvent.setTitle(title);
+        this.title = title;
     }
 
     public void setDescription(String description) {
-        newEvent.setDescription(description);
+        this.description = description;
     }
 
-    public void setStartingDate(String date) {
-        System.out.println("[fra]"+date+"[fra]");
-        //startDateTime = Calendar.getInstance().set(year, month, date);
+    public void setStartDate(String date) {
+        this.startDate = date;
     }
 
-    public void setEndDateTime(String date) {
-        
+    public void setEndDate(String date) {
+        this.endDate = date;
     }
 
     public void setLocation(String location) {
-        newEvent.setLocation(location);
+        this.location = location;
     }
 
     public void setOutdoor(boolean isOutdoor) {
-        newEvent.setIsOutdoor(isOutdoor);
+        this.outdoor = isOutdoor;
+    }
+    
+    public void setPublicAccess(boolean isPublic){
+        this.publicAccess = isPublic;
+    }
+    
+    public void setStartTime(String time){
+        this.startTime = time;
     }
 
+    public void setEndTime(String time){
+        this.endTime = time;
+    }
+    
     public void setInCalendar(String calendarName) {
-
         List<CalendarModel> calendars = calendarManager.getCalendars(login.getCurrentUser());
         if (calendarName != null) {
             calendar = findCalendarByName(calendars, calendarName);
-        }
+        } 
     }
-    
-    private CalendarModel findCalendarByName(List<CalendarModel> calendars, String name){
-        for(CalendarModel cal: calendars){
-            if(cal.getTitle().equals(name)){
+
+    private CalendarModel findCalendarByName(List<CalendarModel> calendars, String name) {
+        for (CalendarModel cal : calendars) {
+            if (cal.getTitle().equals(name)) {
                 return cal;
             }
         }
         return null;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public boolean isOutdoor() {
+        return outdoor;
+    }
+
+    public boolean isPublicAccess() {
+        return publicAccess;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
     
-    public void save(){
+    public String getTitle() {
+        return title;
+    }
+
+    public void save() {
+        if(publicAccess){
+            eventToCreate = new PublicEvent();
+        }else{
+            eventToCreate = new PrivateEvent();
+        }
+        
+        startDateTime = Calendar.getInstance();
+        String[] startDateToken = startDate.split("-");
+        String[] startTimeToken = startTime.split(":");
+        startDateTime.set(Integer.parseInt(startDateToken[0]),
+                          Integer.parseInt(startDateToken[1]),
+                          Integer.parseInt(startDateToken[2]),
+                          Integer.parseInt(startTimeToken[0]),
+                          Integer.parseInt(startTimeToken[1]), 0);
+        
+        endDateTime = Calendar.getInstance();
+        String[] endDateToken = startDate.split("-");
+        String[] endTimeToken = startTime.split(":");
+        endDateTime.set(Integer.parseInt(endDateToken[0]),
+                          Integer.parseInt(endDateToken[1]),
+                          Integer.parseInt(endDateToken[2]),
+                          Integer.parseInt(endTimeToken[0]),
+                          Integer.parseInt(endTimeToken[1]), 0);  
+        
+        eventToCreate.setDescription(description);
+        eventToCreate.setTitle(title);
+        eventToCreate.setLocation(location);
+        eventToCreate.setIsOutdoor(outdoor);
+        eventToCreate.setStartDateTime(startDateTime);
+        eventToCreate.setEndDateTime(endDateTime);
+        eventToCreate.setOwner(login.getCurrentUser());
+        eventManager.scheduleNewEvent(login.getCurrentUser(), eventToCreate, calendar, null);
     }
 
 }
